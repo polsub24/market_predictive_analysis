f910cdd785798759d238fd690188671276fa381a
polsub24 <poltu2108@gmail.com>
2024-09-21 09:48:22 +0530

    third

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template, url_for
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # Load innovation data
 innovation_data = pd.read_csv('data/innovation_data.csv')
+
+# Create a linear regression model to predict innovation score
+X = innovation_data[['rd_spend', 'product_launch_success']]  # use rd_spend and product_launch_success as features
+y = innovation_data['product_launch_success']  # use product_launch_success as the target variable
+
+model = LinearRegression()
+model.fit(X, y)
 
 @app.route('/')
 def visualize():
@@ -30,9 +38,15 @@ def visualize():
     plt.savefig('static/scatter_plot.png')
     plt.close(fig)  # Close the figure
 
+    # Predict the best company for innovation
+    predicted_innovation_scores = model.predict(X)
+    best_company_index = predicted_innovation_scores.argmax()
+    best_company_name = innovation_data.iloc[best_company_index]['company_name']
+
     return render_template('visualize.html', 
                            bar_chart=url_for('static', filename='bar_chart.png'), 
-                           scatter_plot=url_for('static', filename='scatter_plot.png'))
+                           scatter_plot=url_for('static', filename='scatter_plot.png'),
+                           best_company_name=best_company_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
